52449d7a9502f48895eb417203335a662ff1ef63
unyikrisz <krisztian@chrome.hu>
2017-02-12 19:56:43 +0100

    bootstrap

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
-include "index.html";
+
+$url = isset($_GET['_url'])?$_GET['_url']:'/';
+switch ($url){
+    case '/':
+        include "index.html";
+        break;
+    case '/index':
+        include "index.html";
+        break;
+    case '/bemutatkozas':
+        include "page.html";
+        break;
+    case '/referencia':
+        include "page.html";
+        break;
+    case '/kapcsolat':
+        include "page.html";
+        break;
+    case '/express_szallitmanyozas':
+        include "page.html";
+        break;
+    case '/gyujto_fuvarozas':
+        include "page.html";
+        break;
+    default:
+        include "404.html";
+        break;
+}
+//include "index.html";
